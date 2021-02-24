563b54b9e22a11e93dc77d4e5c06759d0558e4f5
Michael Roth <z3r0privacy@users.noreply.github.com>
2021-02-24 11:47:47 +0100

    Update gotya_rd.ps1

--- gotya_rd.ps1
+++ gotya_rd.ps1
@@ -3,4 +3,5 @@ $usr = $env:username
 $name = ([adsi]"WinNT://$dom/$usr,user").fullname
 
 $msg = "Hello $name. I am a script that has just been downloaded after you plugged in a malicious usb device. In the future, please be more careful!"
-[System.Windows.MessageBox]::Show($msg)
+Add-Type -AssemblyName System.Windows.Forms
+[System.Windows.Forms.MessageBox]::Show($msg)
